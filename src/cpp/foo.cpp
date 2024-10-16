22989715e3ff2dc7ab7af892a1d4238640b03ed8
MURAKI Taichi <muraki.taichi@gmail.com>
2024-10-16 16:36:34 +0900

    fix

--- src/cpp/foo.cpp
+++ src/cpp/foo.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 
-extern "C" int foo()
+int foo()
 {
     std::cout << "Func foo." << std::endl;
     return 0;
 }
+
+extern int __real_foo();
+
+int __wrap_foo()
+{
+    std::cout << "Func start wrap foo." << std::endl;
+    __real_foo();
+    std::cout << "Func wrapped." << std::endl;
+    return 0;
+}
+
+#ifdef __cplusplus
+}
+#endif
--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,16 +1,5 @@
 #include <iostream>
-
-extern "C" int foo();
-extern "C" int __real_foo();
-
-extern "C"
-int __wrap_foo()
-{
-    std::cout << "Func start wrap foo." << std::endl;
-    __real_foo();
-    std::cout << "Func wrapped." << std::endl;
-    return 0;
-}
+#include "foo.hpp"
 
 int main()
 {
